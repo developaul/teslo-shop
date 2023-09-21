0e626134f941016a5704143124f34a2ebce21b3f
Paul Chávez <paulchavezromero20@gmail.com>
2023-09-20 20:53:38 -0500

    chore: FullScreenLoading added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import '@/styles/globals.css'
-import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
 import { lightTheme } from '@/themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme} >
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <ThemeProvider theme={lightTheme} >
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { Typography } from "@mui/material"
 
 import { ShopLayout } from "@/components/layouts"
 import { ProductList } from "@/components/products"
-
-import { initialData } from "@/database/products"
+import { FullScreenLoading } from "@/components/ui"
+import { useProducts } from "@/hooks"
 
 
 const HomePage: NextPage = () => {
+  const { products, isLoading } = useProducts('/products')
+
   return (
     <ShopLayout
       title={"Teslo-Shop - Home"}
@@ -16,7 +18,11 @@ const HomePage: NextPage = () => {
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }} >Todos los Productos</Typography>
 
-      <ProductList products={initialData.products as any} />
+      {
+        isLoading
+          ? <FullScreenLoading />
+          : <ProductList products={products} />
+      }
 
     </ShopLayout>
   )
